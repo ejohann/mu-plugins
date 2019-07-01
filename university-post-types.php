f690f6fb611e8b277632144384f83e724573ed8b
Johanne <johanne_lewis@hotmail.com>
2019-07-01 17:51:14 +0100

    Change label for single event and multiple events

--- university-post-types.php
+++ university-post-types.php
@@ -5,7 +5,10 @@
 			'public' => true,
 			'labels' => array(
 				'name' => 'Events',
-				'add_new_item' => 'Add New Event'
+				'add_new_item' => 'Add New Event',
+				'edit_item' => 'Edit Event',
+				'all_items' => 'All Events',
+				'singular_name' => 'Event'
 			),
 			'menu_icon' => 'dashicons-calendar'
 		));
